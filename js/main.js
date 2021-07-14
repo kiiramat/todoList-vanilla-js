6ff42bafd1abb439e98aa9a12cae584bde1e6852
kiiramat <kiiramati@gmail.com>
2021-07-14 20:52:22 +0100

    refactored code to show back to top button when 200px are scrolled vertically

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ todo.draw();
 addEventListener("scroll", () => {
     const scrollToTopBtn = document.querySelector(".scroll-to-top-button");
     let initialWindowHeight = window.scrollY;
-    if (initialWindowHeight > 150) {
+    if (initialWindowHeight > 200) {
         scrollToTopBtn.classList.remove("hidden");
     } else {
         scrollToTopBtn.classList.add("hidden");
